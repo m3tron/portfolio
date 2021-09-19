b3f93eaa3ae7624eb72b6c413b5a4b1c24fffb54
Sunny <sunny.heyar@gmail.com>
2021-09-19 18:11:30 -0400

    starting for scratch

--- components/Header.js
+++
@@ -1,9 +0,0 @@
-const Header = () => (
-  <header>
-    <nav>
-      <a href="#">Home</a>
-    </nav>
-  </header>
-);
-
-export default Header;
--- components/Hero.js
+++
@@ -1,7 +0,0 @@
-const Hero = () => (
-  <section id="hero" className="section">
-    <h1>Sunny Heyar</h1>
-  </section>
-);
-
-export default Hero;
