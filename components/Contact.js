85802700d2a6b61763049a7b640741aeaf8fa138
Sunny Heyar <sunny.heyar@gmail.com>
2022-08-14 23:27:06 -0400

    added inputs to contact section

--- components/Contact.js
+++ components/Contact.js
@@ -1,9 +1,33 @@
 import styles from "../styles/Contact.module.css";
 
 const Contact = () => {
+  const onSubmit = event => {
+    event.preventDefault();
+    console.log("submitted");
+  };
+
   return (
-    <section id="contact">
+    <section id="contact" className={styles.contact}>
       <div className={styles.title}>Contact</div>
+      <div>Use the form or email me at sunny.heyar@gmail.com</div>
+      <form onSubmit={onSubmit} className={styles.form}>
+        <label for="name">Name</label>
+        <input type="text" id="name" placeholder="Please enter your name..." />
+
+        <label for="email">E-mail</label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Please enter your e-mail..."
+        />
+
+        <label for="message">Message</label>
+        <textarea id="message" placeholder="Leave me a message..." />
+
+        <button className={styles.submitButton} type="submit">
+          Submit
+        </button>
+      </form>
     </section>
   );
 };
