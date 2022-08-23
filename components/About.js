808b0d6402e088c146e0f85ffaaf1ce54db9e3a6
Sunny Heyar <sunny.heyar@gmail.com>
2022-08-23 13:13:55 -0400

    added shadow to profile picture

--- components/About.js
+++ components/About.js
@@ -3,7 +3,15 @@ import styles from "../styles/About.module.css";
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section id="about" className={styles.about}>
+      <div className={styles.title}>About Me</div>
+      <div className={styles.information}>
+        <img src="me.png" alt="My face" className={styles.image} />
+        <div className={styles.description}> Description about me</div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
