cb33ac635e684ea52004c15acaf80d4b6a57fee5
Sunny Heyar <sunny.heyar@gmail.com>
2022-08-23 11:30:25 -0400

    added alt description to hero background image

--- components/Hero.js
+++ components/Hero.js
@@ -3,7 +3,11 @@ import styles from "../styles/Hero.module.css";
 const Hero = () => {
   return (
     <div id="home" className={styles.header}>
-      <img src="/mountain_large.jpg" className={styles.background} />
+      <img
+        src="/mountain_large.jpg"
+        className={styles.background}
+        alt="Cloudy mountain"
+      />
       <div className={styles.hero}>
         <div className={styles.typingContainer}>
           <div className={styles.typed1}>Hello World!</div>
