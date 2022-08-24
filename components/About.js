a0fcb60d0541b5b44ed0b79f2fd2737cd973896b
Sunny Heyar <sunny.heyar@gmail.com>
2022-08-24 18:31:21 -0400

    added a description to about section

--- components/About.js
+++ components/About.js
@@ -1,7 +1,5 @@
 import styles from "../styles/About.module.css";
 
-import React from "react";
-
 const About = () => {
   return (
     <section id="about" className={styles.about}>
@@ -9,7 +7,18 @@ const About = () => {
       <div className={styles.information}>
         <img src="me.png" alt="My face" className={styles.image} />
         <div className={styles.description}>
-          <p>I'm a MERN stack developer</p>
+          <p>
+            I'm a full stack web developer currently based in Toronto with
+            experience working remotely.
+          </p>
+          <p>
+            I mostly develop using the MERN stack, but I'm highly adaptable to
+            any tool or technology a project requires.
+          </p>
+          <p>
+            When I'm not coding, I like to workout, play volleyball, and ride or
+            work on my motorcycle.
+          </p>
         </div>
       </div>
     </section>
